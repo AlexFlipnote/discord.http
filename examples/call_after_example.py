bf8f0e6526a4266c680a6aea3082da25f9e39f3b
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2025-02-15 09:06:24 +0100

    More examples

---
+++ examples/call_after_example.py
@@ -0,0 +1,25 @@
+import asyncio
+
+from discord_http import Context, Client
+
+client = Client(
+    token="BOT_TOKEN",
+    application_id=1337,
+    public_key="PUBLIC_KEY",
+    sync=True
+)
+
+
+@client.command()
+async def followup_example(ctx: Context):
+    """ My name jeff """
+    async def call_after():
+        await asyncio.sleep(3)
+        await ctx.edit_original_response(
+            content="Hey, I have entered the chat!"
+        )
+
+    return ctx.response.defer(thinking=True, call_after=call_after)
+
+
+client.start(host="127.0.0.1", port=8080)
---
+++ examples/choices_example.py
@@ -0,0 +1,38 @@
+from discord_http import Context, Client, commands
+
+client = Client(
+    token="BOT_TOKEN",
+    application_id=1337,
+    public_key="PUBLIC_KEY",
+    sync=True
+)
+
+
+@client.command()
+@commands.choices(
+    choice={
+        "hello": "Hello there!",
+        "goodbye": "Goodbye!"
+    }
+)
+async def choices_str(ctx: Context, choice: commands.Choice[str]):
+    return ctx.response.send_message(
+        f"You chose **{choice.value}** which has key value: **{choice.key}**"
+    )
+
+
+@client.command()
+@commands.choices(
+    choice={
+        23: "Nice",
+        55: "meme"
+    }
+)
+async def choices_int(ctx: Context, choice: commands.Choice[int]):
+    return ctx.response.send_message(
+        f"You chose **{choice.value}** ({type(choice.value)}) "
+        f"which has key value: **{choice.key}** ({type(choice.key)})"
+    )
+
+
+client.start(host="127.0.0.1", port=8080)
