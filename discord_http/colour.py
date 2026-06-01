c22089977fbdd0e98e6564bf2fa386382c40cc1e
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2026-06-01 13:21:42 +0200

    Make the colour be more versatile

--- discord_http/colour.py
+++ discord_http/colour.py
@@ -53,6 +53,83 @@ class Colour:
         """ Returns the blue component of the colour. """
         return self.value & 0xFF
 
+    @property
+    def brightness(self) -> int:
+        """ Returns the perceived brightness of the colour (0-255). """
+        return (self.r + self.g + self.b) // 3
+
+    def is_dark(self) -> bool:
+        """ Returns whether the colour is considered dark based on perceived luminance. """
+        return (self.r * 0.299 + self.g * 0.587 + self.b * 0.114) <= 186
+
+    def is_light(self) -> bool:
+        """ Returns whether the colour is considered light based on perceived luminance. """
+        return not self.is_dark
+
+    def to_rgb(self) -> tuple[int, int, int]:
+        """ Returns the RGB values of the colour`. """
+        return (self.r, self.g, self.b)
+
+    def to_hex(self) -> str:
+        """ Returns the hex value of the colour. """
+        return f"#{self.value:06x}"
+
+    def to_hsl(self) -> tuple[int, int, int]:
+        """ Returns the HSL (hue 0-360, saturation 0-100, lightness 0-100) values of the colour. """
+        r, g, b = self.r / 255, self.g / 255, self.b / 255
+        cmax, cmin = max(r, g, b), min(r, g, b)
+        delta = cmax - cmin
+
+        if delta == 0:
+            h = 0
+        elif cmax == r:
+            h = 60 * (((g - b) / delta) % 6)
+        elif cmax == g:
+            h = 60 * (((b - r) / delta) + 2)
+        else:
+            h = 60 * (((r - g) / delta) + 4)
+
+        ll = (cmax + cmin) / 2
+        s = 0 if delta == 0 else delta / (1 - abs(2 * ll - 1))
+        return round(h), round(s * 100), round(ll * 100)
+
+    def to_hsv(self) -> tuple[int, int, int]:
+        """ Returns the HSV (hue 0-360, saturation 0-100, value 0-100) values of the colour. """
+        r, g, b = self.r / 255, self.g / 255, self.b / 255
+        cmax, cmin = max(r, g, b), min(r, g, b)
+        delta = cmax - cmin
+
+        if delta == 0:
+            h = 0
+        elif cmax == r:
+            h = 60 * (((g - b) / delta) % 6)
+        elif cmax == g:
+            h = 60 * (((b - r) / delta) + 2)
+        else:
+            h = 60 * (((r - g) / delta) + 4)
+
+        s = 0 if cmax == 0 else delta / cmax
+        return round(h), round(s * 100), round(cmax * 100)
+
+    def to_cmyk(self) -> tuple[int, int, int, int]:
+        """ Returns the CMYK (cyan 0-100, magenta 0-100, yellow 0-100, key/black 0-100) values of the colour. """
+        if (self.r, self.g, self.b) == (0, 0, 0):
+            return 0, 0, 0, 100
+
+        c = 1 - self.r / 255
+        m = 1 - self.g / 255
+        y = 1 - self.b / 255
+        k = min(c, m, y)
+        c = (c - k) / (1 - k)
+        m = (m - k) / (1 - k)
+        y = (y - k) / (1 - k)
+        return (
+            round(c * 100),
+            round(m * 100),
+            round(y * 100),
+            round(k * 100),
+        )
+
     @classmethod
     def from_rgb(cls, r: int, g: int, b: int) -> Self:
         """
@@ -72,10 +149,6 @@ class Colour:
             The colour object
         """
         return cls((r << 16) + (g << 8) + b)
-
-    def to_rgb(self) -> tuple[int, int, int]:
-        """ Returns the RGB values of the colour`. """
-        return (self.r, self.g, self.b)
 
     @classmethod
     def from_hex(cls, hex_value: str) -> Self:
@@ -109,9 +182,108 @@ class Colour:
         except ValueError:
             raise ValueError(f"Invalid hex colour {hex_value!r}")
 
-    def to_hex(self) -> str:
-        """ Returns the hex value of the colour. """
-        return f"#{self.value:06x}"
+    @classmethod
+    def from_hsl(cls, h: int, s: int, l: int) -> Self:  # noqa: E741
+        """
+        Creates a Colour object from HSL values.
+
+        Parameters
+        ----------
+        h:
+            Hue (0-360)
+        s:
+            Saturation (0-100)
+        l:
+            Lightness (0-100)
+
+        Returns
+        -------
+            The colour object
+        """
+        sn = s / 100
+        ln = l / 100
+        c = (1 - abs(2 * ln - 1)) * sn
+        x = c * (1 - abs((h / 60) % 2 - 1))
+        m = ln - c / 2
+
+        if h < 60:
+            r, g, b = c, x, 0
+        elif h < 120:
+            r, g, b = x, c, 0
+        elif h < 180:
+            r, g, b = 0, c, x
+        elif h < 240:
+            r, g, b = 0, x, c
+        elif h < 300:
+            r, g, b = x, 0, c
+        else:
+            r, g, b = c, 0, x
+
+        return cls.from_rgb(round((r + m) * 255), round((g + m) * 255), round((b + m) * 255))
+
+    @classmethod
+    def from_hsv(cls, h: int, s: int, v: int) -> Self:
+        """
+        Creates a Colour object from HSV values.
+
+        Parameters
+        ----------
+        h:
+            Hue (0-360)
+        s:
+            Saturation (0-100)
+        v:
+            Value (0-100)
+
+        Returns
+        -------
+            The colour object
+        """
+        sn = s / 100
+        vn = v / 100
+        c = vn * sn
+        x = c * (1 - abs((h / 60) % 2 - 1))
+        m = vn - c
+
+        if h < 60:
+            r, g, b = c, x, 0
+        elif h < 120:
+            r, g, b = x, c, 0
+        elif h < 180:
+            r, g, b = 0, c, x
+        elif h < 240:
+            r, g, b = 0, x, c
+        elif h < 300:
+            r, g, b = x, 0, c
+        else:
+            r, g, b = c, 0, x
+
+        return cls.from_rgb(round((r + m) * 255), round((g + m) * 255), round((b + m) * 255))
+
+    @classmethod
+    def from_cmyk(cls, c: int, m: int, y: int, k: int) -> Self:
+        """
+        Creates a Colour object from CMYK values.
+
+        Parameters
+        ----------
+        c:
+            Cyan (0-100)
+        m:
+            Magenta (0-100)
+        y:
+            Yellow (0-100)
+        k:
+            Key/black (0-100)
+
+        Returns
+        -------
+            The colour object
+        """
+        r = round(255 * (1 - c / 100) * (1 - k / 100))
+        g = round(255 * (1 - m / 100) * (1 - k / 100))
+        b = round(255 * (1 - y / 100) * (1 - k / 100))
+        return cls.from_rgb(r, g, b)
 
     @classmethod
     def default(cls) -> Self:
@@ -139,9 +311,19 @@ class Colour:
         r = random.Random(str(seed)) if seed else random
         return cls(r.randint(0, 0xFFFFFF))
 
+    # Discord branding colours if needed
+    @classmethod
+    def blurple(cls) -> Self:
+        """ Returns the blurple branding colour of Discord. """
+        return cls(0x5865f2)
+
+    @classmethod
+    def light_blurple(cls) -> Self:
+        """ Returns the light blurple branding colour of Discord. """
+        return cls(0xe0e3ff)
+
     # Colours based on https://flatuicolors.com/palette/defo
     # A few of them are custom to Discord however
-
     @classmethod
     def turquoise(cls) -> Self:
         """ Returns the turquoise colour. """
